481135aabff24f6e0331750ae2ac62a564ffc7f0
3omar.hs <omarhj13702@yahoo.com>
2022-11-28 19:07:45 +0300

    3omar.hs

--- subCode_files/readData.py
+++ subCode_files/readData.py
@@ -15,12 +15,10 @@ class imagesData():
         global allA
         for (dirpath, dirnames, filenames) in walk(mypath):
             for i in range(len(dirnames)):
-                # print(dirpath+'\\'+dirnames[i])
                 childListA = []
                 childListB = []
                 for (ch_dirpath, ch_dirnames, ch_filenames) in walk(dirpath+'\\'+dirnames[i]):
                     for j in range(len(ch_filenames)):
-                        # print(ch_dirpath+'\\'+ch_filenames[j])
                         if 'before' in ch_filenames[j]:
                             childListB.append(ch_dirpath+'\\'+ch_filenames[j])
                         elif 'after' in ch_filenames[j]:
@@ -33,22 +31,12 @@ class imagesData():
                 else:
                     b[dirnames[i]]=childListB
                 allA.extend(childListA)
-                # print(len(a[dirnames[i]]))
             break
         print(a)
         print('*'*85000)
         print(b)
         print('*'*85000)
         print(allA)
-
-        # try readsing images:
-        '''
-        import cv2
-        x = cv2.imread(a['dior'][10])
-        while 1:
-            cv2.waitKey(1)
-            cv2.imshow("preview..", x)
-        '''
 
 if justTest == True:
     imagesData()
